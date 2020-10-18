f29cc3df423fd17351fda0592e5976b7e1e79e29
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-10-18 14:35:47 +0300

    first commit

---
+++ loginPage/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView, View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={mystyle.container}>
+      <View style={mystyle.allbox}>
+
+      </View>
+      
+
+    </SafeAreaView>
+
+
+  )
+}
+
+const mystyle = StyleSheet.create({
+  container: {},
+  allbox:{},
+})
+export default App;
+
